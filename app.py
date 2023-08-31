5b910f3d79370f4f87b46de8e6b120a08be3dd31
Joseph Jabbour <j.jabbour@dq.com.lb>
2023-08-31 13:56:47 +0300

    removed api key

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from langchain.agents.agent_toolkits import (
 
 # Set APIkey for OpenAI Service
 # Can sub this out for other LLM providers
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = ''
 
 # Create instance of OpenAI LLM
 llm = OpenAI(temperature=0.1, verbose=True)
