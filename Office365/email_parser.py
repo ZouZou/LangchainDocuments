d5758fd0ba0896eda7bd0b878024f4004ffedd51
Joseph Jabbour <j.jabbour@dq.com.lb>
2023-09-19 16:24:20 +0300

    working with emails

---
+++ Office365/email_parser.py
@@ -0,0 +1,18 @@
+import datetime
+import json
+import eml_parser
+
+
+def json_serial(obj):
+  if isinstance(obj, datetime.datetime):
+      serial = obj.isoformat()
+      return serial
+
+
+with open('emails/Doc384605_78718.eml', 'rb') as fhdl:
+  raw_email = fhdl.read()
+
+ep = eml_parser.EmlParser()
+parsed_eml = ep.decode_email_bytes(raw_email)
+
+print(json.dumps(parsed_eml, default=json_serial))
